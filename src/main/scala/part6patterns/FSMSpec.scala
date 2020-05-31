7a771502ae10900632ae1afd96123a80ba7680e5
stefan521 <muscalu.stefan@yahoo.com>
2020-05-31 23:20:09 +0100

    Finished the FSM section.

--- src/main/scala/part6patterns/FSMSpec.scala
+++ src/main/scala/part6patterns/FSMSpec.scala
@@ -1,6 +1,6 @@
 package part6patterns
 
-import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Cancellable, Props}
+import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Cancellable, FSM, Props}
 import akka.testkit.{ImplicitSender, TestKit}
 import org.scalatest.{BeforeAndAfterAll, WordSpecLike}
 
@@ -91,6 +91,80 @@ class FSMSpec extends TestKit(ActorSystem("FSMSpec"))
     }
   }
 
+  "A vending machine FSM" should {
+    "error when not initialized" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+
+      vendingMachine ! RequestProduct("coke")
+
+      expectMsg(VendingError("MachineNotInitialized"))
+    }
+
+    "report a product not available" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+
+      vendingMachine ! Initialize(Map("coke" -> 10), Map("coke" -> 1))
+      vendingMachine ! RequestProduct("sandwich")
+
+      expectMsg(VendingError("ProductNotAvailable"))
+    }
+
+    "throw a timeout if I don't insert money" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+      vendingMachine ! Initialize(Map("coke" -> 10), Map("coke" -> 1))
+
+      vendingMachine ! RequestProduct("coke")
+
+      expectMsg(Instruction("Please insert 1 dollars"))
+
+      within(3 seconds) {
+        expectMsg(VendingError("RequestTimedOut"))
+      }
+    }
+
+    "handle the reception of partial money" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+      vendingMachine ! Initialize(Map("coke" -> 10), Map("coke" -> 3))
+
+      vendingMachine ! RequestProduct("coke")
+      expectMsg(Instruction("Please insert 3 dollars"))
+
+      vendingMachine ! ReceiveMoney(1)
+      expectMsg(Instruction("Please insert 2 dollars"))
+
+      within(3 seconds) {
+        expectMsg(VendingError("RequestTimedOut"))
+        expectMsg(GiveBackChange(1))
+      }
+    }
+
+    "a vending machine should deliver the product if I insert all the money" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+      vendingMachine ! Initialize(Map("coke" -> 10), Map("coke" -> 3))
+
+      vendingMachine ! RequestProduct("coke")
+      expectMsg(Instruction("Please insert 3 dollars"))
+
+      vendingMachine ! ReceiveMoney(3)
+      expectMsg(Deliver("coke"))
+    }
+
+    "a vending machine should give back change and be able to request money for a new product" in {
+      val vendingMachine = system.actorOf(Props[VendingMachineFSM])
+      vendingMachine ! Initialize(Map("coke" -> 10), Map("coke" -> 3))
+
+      vendingMachine ! RequestProduct("coke")
+      expectMsg(Instruction("Please insert 3 dollars"))
+
+      vendingMachine ! ReceiveMoney(5)
+      expectMsg(Deliver("coke"))
+      expectMsg(GiveBackChange(2))
+
+      vendingMachine ! RequestProduct("coke")
+      expectMsgType[Instruction]
+    }
+  }
+
 }
 
 object FSMSpec {
@@ -162,6 +236,88 @@ object FSMSpec {
     def startReceiveMoneyTimeoutSchedule: Cancellable = context.system.scheduler.scheduleOnce(1 second) {
       self ! ReceiveMoneyTimeout
     } (context.dispatcher)
+  }
+
+  // step 1 - define the states and the data of the actor
+  trait VendingState
+  case object Idle extends VendingState
+  case object Operational extends VendingState
+  case object WaitForMoney extends VendingState
+
+  trait VendingData
+  case object Uninitialized extends VendingData
+  case class Initialized(
+    inventory: Map[String, Int],
+    prices: Map[String, Int]
+  ) extends VendingData
+  case class WaitForMoneyData(
+     inventory: Map[String, Int],
+     prices: Map[String, Int],
+     product: String,
+     money: Int, // inserted so far
+     requester: ActorRef
+   ) extends VendingData
+
+  class VendingMachineFSM extends FSM[VendingState, VendingData] {
+    // in FSM we don't have a receive handler
+
+    // an EVENT(message, data)
+    startWith(Idle, Uninitialized)
+
+    when(Idle) {
+      case Event(Initialize(inventory, prices), Uninitialized) =>
+        goto(Operational) using Initialized(inventory, prices)
+      case _ =>
+        sender ! VendingError("MachineNotInitialized")
+        stay
+    }
+
+    when(Operational) {
+      case Event(RequestProduct(product), Initialized(inventory, prices)) => inventory.get(product) match {
+        case None | Some(0) =>
+          sender ! VendingError("ProductNotAvailable")
+          stay
+        case Some(_) =>
+          val price = prices(product)
+          sender ! Instruction(s"Please insert $price dollars")
+          goto(WaitForMoney) using WaitForMoneyData(inventory, prices, product, 0, sender)
+      }
+    }
+
+    when(WaitForMoney, stateTimeout = 2 second) {
+      case Event(StateTimeout, WaitForMoneyData(inventory, prices, _, money, requester)) =>
+        requester ! VendingError("RequestTimedOut")
+        if (money > 0) requester ! GiveBackChange(money)
+        goto(Operational) using Initialized(inventory, prices)
+
+      case Event(ReceiveMoney(amount), WaitForMoneyData(inventory, prices, product, money, requester)) =>
+        val price = prices(product)
+        if (money + amount >= price) {
+          requester ! Deliver(product)
+          if (money + amount - price > 0) requester ! GiveBackChange(money + amount - price)
+          val newStock = inventory(product) - 1
+          val newInventory = inventory + (product -> newStock)
+
+          goto(Operational) using Initialized(newInventory, prices)
+        } else {
+          val remainingMoney = price - money - amount
+          requester ! Instruction(s"Please insert $remainingMoney dollars")
+
+          stay using WaitForMoneyData(inventory, prices, product, money + amount, requester)
+        }
+    }
+
+    whenUnhandled {
+      case Event(_, _) =>
+        sender ! VendingError("CommandNotFound")
+        stay
+    }
+
+    onTransition {
+      case stateA -> stateB => log.info(s"Transitioning from $stateA to $stateB")
+    }
+
+    initialize() // starts the actor
   }
 
 }
